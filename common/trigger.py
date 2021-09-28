2d4cacf2244cdbe120e8fafe2d0579198c85e516
Rio McMahon <rmcmahon@ucar.edu>
2021-09-28 10:52:42 -0600

    refactored stuff, still having issues with systemd but technically this works. you need to still manually start edex server and activate edex side of grpc server

--- common/trigger.py
+++ common/trigger.py
@@ -1,7 +1,14 @@
 import socket
-def send(msg, host, port):
+import sys
+
+if __name__ == '__main__':
+    msg = sys.argv[1]
+    host = "tfc"
+    port = 7000
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.sendall(bytes(msg, 'utf-8'))
         data = s.recv(1024)
         print(data)
+
+    
