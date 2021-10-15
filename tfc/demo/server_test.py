249792b64a6896d071041a17e0599b2109ba8e9d
Rio McMahon <rmcmahon@ucar.edu>
2021-10-15 14:13:11 -0600

    started breaking out functions into tf server IO layer

--- tfc/demo/server_test.py
+++ tfc/demo/server_test.py
@@ -11,15 +11,24 @@ async def make_request(url, data):
             response_json = await response.json()
             return np.array(response_json['predictions'])
 
+def get_payload(nc, variable_spec):
+    data = nc.variables[variable_spec][:].data
+    data = data.reshape((1, *data.shape))
+    return f'{{"instances" : {data.tolist()}}}'
+
+
 # use this file to test interaction with tf server
 if __name__ == '__main__':
+
+    # get data from file
     fp = "/Users/rmcmahon/dev/awips-ml/tfc/demo/OR_ABI-L2-CMIPPR-M6C09_G16_s20212861650200_e20212861650200_c20212861650200.nc4"
     nc = nc4.Dataset(fp)
-    array = nc.variables['Sectorized_CMI'][:].data.reshape(1,890, 976)
+
+    # setup POST stuff
+    payload = get_payload(nc, "Sectorized_CMI")
+    url = 'http://localhost:8501/v1/models/model:predict'
 
     # POST to server via http
-    url = 'http://localhost:8501/v1/models/model:predict'
-    payload = f'{{"instances" : {array.tolist()}}}'
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(make_request(url, payload))
+    model_response = asyncio.run(make_request(url, payload))
+    print(model_response)
 
