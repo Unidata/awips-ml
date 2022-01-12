24547f307df88d3f9a5e82f8c393e80f13f13418
Rio McMahon <rmcmahon@ucar.edu>
2022-01-12 10:05:23 -0700

    added architecture diagram for wiki

--- usr/postproc.py
+++ usr/postproc.py
@@ -1,2 +1,10 @@
 def postproc(model_output):
+    '''
+    Function for custom user postprocessing.
+
+    Takes `model_output` which is a numpy array corresponding to the machine
+    learning model output. `postproc` should return a numpy array with similar
+    dimensions for transmission to and ingestion by the EDEX container.
+    '''
+    # input custom code here...
     return model_output
