5b754689d42d0641a25ab87b5e1104956d17cbcb
Rio McMahon <rmcmahon@ucar.edu>
2022-01-20 08:32:37 -0700

    small dockerfile mods to bring up to speed with main

--- edexc/etc/systemd/edex_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-/usr/bin/edex start
-/usr/bin/rm -rf /awips2/edex/data/hdf5/satellite
-/awips2/psql/bin/psql -U awips -c "delete from satellite;" metadata
-/awips2/psql/bin/psql -U awips -c "delete from satellite_spatial;" metadata
